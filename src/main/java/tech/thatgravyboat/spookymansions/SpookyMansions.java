c62f484f0c539dced0e84174bcefebd6ea59fa8a
Gravy Boat <gravy@thatgravyboat.tech>
2025-10-02 06:16:58 -0230

    Initial Commit

---
+++ src/main/java/tech/thatgravyboat/spookymansions/SpookyMansions.java
@@ -0,0 +1,14 @@
+package tech.thatgravyboat.spookymansions;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SpookyMansions.MODID)
+public class SpookyMansions {
+    public static final String MODID = "spookymansions";
+
+    public SpookyMansions(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
